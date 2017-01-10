4109d3e93e0ea425283f643751c730a484d791d6
OOuissem <smartmen0909@gmail.com>
2017-01-10 16:35:47 +0100

    version 4

--- allongons_les_gembes.rb
+++ allongons_les_gembes.rb
@@ -3,7 +3,7 @@ ch2 = " poussins"
 ch3 = "Elle en avait trente.\n"
 ch4 ="Allongeons la jambe, \n Allongeons la jambe, la jambe.\n Car la route est longue.\n"
 
-30.times do |i|
-    ch = ch1 + String(i) + ch2 +",\n" + ch1 + String(i) + ch2 +".\n" + ch3  + ch4 + ch4    
+29.times do |i|
+    ch = ch1 + String(30-i) + ch2 +",\n" + ch1 + String(30-i) + ch2 +".\n" + ch3  + ch4 + ch4 + "\n\n"    
     puts ch
 end
