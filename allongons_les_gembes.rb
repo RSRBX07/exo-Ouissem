cb6c1a6d44fa43fda2f60eb8e1c3115dd42f0456
OOuissem <smartmen0909@gmail.com>
2017-01-10 16:30:54 +0100

    version 2

---
+++ allongons_les_gembes.rb
@@ -0,0 +1,10 @@
+ch1 = "Ma poul' n'a plus que "
+ch2 = " poussins"
+ch3 = "Elle en avait trente.\n"
+ch4 ="Allongeons la jambe, \n Allongeons la jambe, la jambe.\n Car la route est longue.\n"
+
+30.times |i|
+{
+    ch = ch1 + String(i) + ch2 +",/n" + ch1 + String(i) + ch2 +"./n" + ch3  + ch4 + ch4    
+    puts ch
+}
