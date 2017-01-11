b24fecf48e23292d96471e7a8f8b9323bf895ebf
OOuissem <smartmen0909@gmail.com>
2017-01-11 16:35:36 +0100

    roll cheated

--- dice.rb
+++ dice.rb
@@ -1,10 +1,15 @@
 
 # retourne la valeur de la face lancée
-def roll
-    faces_values=[1,2,3,4,5,6]
-    return faces_values[rand 6]
+def roll(*num)
+    if num.length == 1
+        return num
+    else
+        faces_values=[1,2,3,4,5,6]
+        return faces_values[rand 6]
+    end
 end
 
 face=roll
-
-print "face jouée : #{face}"
+face_cheated=roll 5
+print "face jouée : #{face} \n"
+print "face pipé : #{face_cheated}"
