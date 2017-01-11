4435a7b8b9d73e48d960e1c163e30e1367bc8e11
OOuissem <smartmen0909@gmail.com>
2017-01-11 13:57:48 +0100

    first fonctional version
    saisie conditionné

--- loto.rb
+++ loto.rb
@@ -3,11 +3,17 @@ all.shuffle!
 repartition=[all[0..8],all[9..17],all[18..26],all[27..35],all[36..44]]
 
 grille = []
-print "tapez vos 5 valeurs choisis : "
+print "tapez vos 5 valeurs choisis : \n"
 
 (1..5).each do |i|
-    print("valeur n° #{i} :")
-    grille.push gets.to_i
+    begin
+        print("valeur n° #{i} :")
+        value = gets.to_i
+        if(value <1)||(value>45)
+            puts "nombre non accepté (il faut choisir un nombre entre 1 et 45)"
+        end
+    end while (value <1)||(value>45)
+    grille.push value
 end
 
 picked=(1..45).to_a.shuffle.take 5
