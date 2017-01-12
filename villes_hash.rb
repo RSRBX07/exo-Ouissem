8d81afee1520848e14b76c8c865c1e93550c53f4
OOuissem <smartmen0909@gmail.com>
2017-01-12 11:35:31 +0100

    version 1 liste villes rectifiée

--- villes_hash.rb
+++ villes_hash.rb
@@ -1,15 +1,15 @@
 liste_villes = [
     {   "p_code" => 59000 ,
-        "ville"   => "Lille" 
+        "nom_ville"   => "Lille" 
     },    
     {   "p_code" => 59100 ,
-        "ville"   => "Roubaix"     
+        "nom_ville"   => "Roubaix"     
     },   
     {   "p_code" => 59170 ,
-        "ville"   => "Croix"
+        "nom_ville"   => "Croix"
     },
     {   "p_code" => 59200 ,
-        "ville"   => "tourcoing" 
+        "nom_ville"   => "tourcoing" 
     }    
 ]
 
@@ -21,7 +21,7 @@ p_code_saisi = gets.chomp.to_i
 puts
 liste_villes.each{|ville| 
     if ville['p_code']==p_code_saisi
-        print "#{ville['ville']}"
+        print "#{ville['nom_ville']}"
         break
     end 
 }
