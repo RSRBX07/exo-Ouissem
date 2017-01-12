7e8187d17ae7b033b573dc51325bcfa9d795b22e
OOuissem <smartmen0909@gmail.com>
2017-01-12 13:10:54 +0100

    version erronée
    >ruby -v villes_hash.rb
    ruby 2.2.5p319 (2016-04-26 revision 54774) [i386-mingw32]
    villes_hash.rb:13: warning: assigned but unused variable - nom_ville_saisi
    choisir un nom de ville parmis cette liste :   lille | roubaix | croix | tourcoing |

--- villes_hash.rb
+++ villes_hash.rb
@@ -1,29 +1,20 @@
-liste_villes = [
-    {   "p_code" => 59000 ,
-        "nom_ville"   => "Lille" 
-    },    
-    {   "p_code" => 59100 ,
-        "nom_ville"   => "Roubaix"     
-    },   
-    {   "p_code" => 59170 ,
-        "nom_ville"   => "Croix"
-    },
-    {   "p_code" => 59200 ,
-        "nom_ville"   => "tourcoing" 
-    }    
-]
+liste_villes = {   
+    lille: 59000 ,               
+    roubaix: 59100 ,               
+    croix: 59170 ,
+    tourcoing: 59200     
+}    
 
 
-print "choisir un code postal parmis cette liste :   "
-liste_villes.each{|ville| print "#{ville['p_code']} | "}
-puts
-p_code_saisi = gets.chomp.to_i
-puts
-liste_villes.each{|ville| 
-    if ville['p_code']==p_code_saisi
-        print "#{ville['nom_ville']}"
-        break
-    end 
-}
 
-puts
+print "choisir un nom de ville parmis cette liste :   "
+liste_villes.each_key{|nom_ville| print "#{nom_ville} | "}
+2.times {puts}
+nom_ville_saisi = gets.chomp
+
+ville_sym = :nom_ville_saisi
+if liste_villes[ville_sym]
+    puts "le code postal correspondant à cette ville est : #{ liste_villes[ville_sym] }"
+else
+    puts "Ville non existante dans la liste!!!" 
+end
