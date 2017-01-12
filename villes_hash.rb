170d535a96719916374acaab8dcdbd9a91e9d256
OOuissem <smartmen0909@gmail.com>
2017-01-12 13:19:29 +0100

    version 1 rectifié fonctionnel

--- villes_hash.rb
+++ villes_hash.rb
@@ -10,9 +10,8 @@ liste_villes = {
 print "choisir un nom de ville parmis cette liste :   "
 liste_villes.each_key{|nom_ville| print "#{nom_ville} | "}
 2.times {puts}
-nom_ville_saisi = gets.chomp
+ville_sym = gets.chomp.to_sym
 
-ville_sym = :nom_ville_saisi
 if liste_villes[ville_sym]
     puts "le code postal correspondant à cette ville est : #{ liste_villes[ville_sym] }"
 else
