0889520dcbfc1ef0d0a21f179b168222b283c2e7
OOuissem <smartmen0909@gmail.com>
2017-01-12 14:14:31 +0100

    version 1 beta

---
+++ french_numbers.rb
@@ -0,0 +1,18 @@
+def saisir msg
+    print(msg)
+    return gets().chomp.to_i
+end
+
+def convertir nombre
+    nb_milliards = nombre / 1000000000
+    nb_millions = nombre % 1000000000 / 1000000
+    nb_milliers = nombre % 1000000 / 1000
+    reste = nombre % 1000
+    return String(nb_milliards) + "milliards et " + String(nb_millions) + "millions et " + String(nb_milliers) + "milles et " + String(reste) 
+end
+
+nb=saisir "taper un nombre : "
+
+tte_lettres = convertir(nb)
+
+puts "le nombre #{nb} en toutes lettres est : #{tte_lettres}"
