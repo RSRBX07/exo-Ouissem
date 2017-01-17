f44ff851d8a74f37c7a2e13c3114e889f822f4d6
OOuissem <smartmen0909@gmail.com>
2017-01-17 15:34:22 +0100

    create rspec environement

---
+++ spec/loto_spec.rb
@@ -0,0 +1,21 @@
+require "./loto/loto.rb"
+# parties = {lundi: nil, Mardi: nil, mercredi: nil, jeudi: nil, vendredi: nil, samedi: nil, dimanche: nil}
+# lotos = {lundi: Loto.new, Mardi: Loto.new, mercredi: Loto.new, jeudi: Loto.new, vendredi: Loto.new, samedi: Loto.new, dimanche: Loto.new}
+# # une seule partie correspondante au Lundi
+# parties[:lundi]= Party.new lotos[:lundi]
+# parties[:lundi].add_grid
+# parties[:lundi].add_grid
+
+# lotos[:lundi].picking
+
+# #test de add_grid après tirage
+# parties[:lundi].add_grid
+
+# lotos[:lundi].show_grids
+# 0.upto parties[:lundi].grid_number-1 do |i|
+#     parties[:lundi].validate_grid i,lotos[:lundi].grid
+# end
+
+# gets
+
+# gets
