b48d83663f871daad0527b00a2cc190e312eb353
OOuissem <smartmen0909@gmail.com>
2017-01-18 17:53:27 +0100

    add plane and vehicle class with @@counter

--- Vehicle/vehicle.rb
+++ Vehicle/vehicle.rb
@@ -1,15 +1,28 @@
 class Vehicle
-    attr_reader :start_position , :target_position
-
+    attr_reader :position , :state
+    @@counter=0;
+    
+    def self.new ville=nil
+         puts "--->  Instanciating vehicle is started   <----"
+         super
+         puts "--->  Instanciating vehicle is finished   <----"         
+         @position = ville
+         @@counter +=1
+         super
+    end
+    
     def initialize ville=nil
-        @start_position = ville
+        puts "--->  Initialization started   <----"
+        @position = ville
+        @state = "Stopped"
+        puts "--->  Initialization finished   <----"
     end
 
     def set_position ville
         @position = ville
-    end
+    end   
 
-    def move ville
-        @target_position = ville
+    def Vehicle.counter
+        @@counter
     end
 end
--- test.rb
+++ test.rb
@@ -1,6 +1,24 @@
 require "./vehicle/vehicle.rb"
+require "./vehicle/plane.rb"
 
-my_vehicle = Vehicle.new "Lille"
-my_vehicle.move "Paris"
 
-puts my_vehicle.start_position + "  >>>>>>  " + my_vehicle.target_position
+my_plane = Plane.new "Lille"
+## oubien
+#my_plane_2 = Plane.new
+#my_plane_2.set_position "Lille"
+
+puts "Position actuelle   >>>>>>  " + my_plane.position
+
+puts "etat 1 : " + my_plane.state + "  --- roue sorties :" + my_plane.wheel_out
+
+my_plane.take_off
+puts "etat 2 : " + my_plane.state + "  --- roue sorties :" + my_plane.wheel_out
+
+my_plane.move
+puts "etat 3 : " + my_plane.state + "  --- roue sorties :" + my_plane.wheel_out
+
+my_plane.land
+puts "etat 4 : " + my_plane.state + "  --- roue sorties :" + my_plane.wheel_out
+
+my_plane.stop
+puts "etat 5 : " + my_plane.state + "  --- roue sorties :" + my_plane.wheel_out
