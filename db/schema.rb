077cc69748c1366657f372936885ad250c5761d4
OOuissem <smartmen0909@gmail.com>
2017-01-24 11:46:26 +0100

    add game table, add players field to game, add seed file to edit some table lines in game table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123161225) do
+ActiveRecord::Schema.define(version: 20170124104209) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "players"
   end
 
 end
